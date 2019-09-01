8b6592b87a4ed51b8627a775f4c9b6b43a48b7b9
huisunan <571101320@qq.com>
2019-09-01 22:24:29 +0800

    Ready

--- server/pt-service-bms[9100]/target/docker/src/main/java/com/rsdtm/supermarket/bms/dao/ProductCategoryDao.java
+++
@@ -1,7 +0,0 @@
-package com.rsdtm.supermarket.bms.dao;
-
-import com.rsdtm.supermarket.mbg.model.BmsProductCategory;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ProductCategoryDao extends Mapper<BmsProductCategory> {
-}
--- server/pt-service-bms[9100]/target/docker/src/main/java/com/rsdtm/supermarket/bms/dao/SupplierDao.java
+++
@@ -1,7 +0,0 @@
-package com.rsdtm.supermarket.bms.dao;
-
-import com.rsdtm.supermarket.mbg.model.BmsSupplier;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SupplierDao extends Mapper<BmsSupplier> {
-}
